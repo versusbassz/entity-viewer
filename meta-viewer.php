6c80f3a5f8211e62e8c6a526703b8cc237d11c43
Vladimir Sklyar <versus.post@gmail.com>
2021-05-29 18:29:51 +0300

    #17 Add an admin notice for installations with old PHP versions

--- meta-viewer.php
+++ meta-viewer.php
@@ -6,8 +6,21 @@ Requires PHP: 7.3
 
 defined('ABSPATH') || exit;
 
-if (version_compare(PHP_VERSION, '7.3.0', '>=')) {
-    add_action('admin_init', 'vs_init_meta_viewer_plugin');
+vs_start_meta_viewer_plugin();
+
+function vs_start_meta_viewer_plugin() {
+	if (! version_compare(PHP_VERSION, '7.3.0', '>=')) {
+		add_action('admin_notices', 'vs_display_php_requirement_notice_for_meta_viewer_plugin');
+	}
+
+	add_action('admin_init', 'vs_init_meta_viewer_plugin');
+}
+
+function vs_display_php_requirement_notice_for_meta_viewer_plugin() {
+	$class = 'notice notice-error is-dismissible';
+	$message = 'The plugin "Meta viewer" doesn\'t support your PHP version and isn\'t initialized because of that.';
+
+	printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 }
 
 function vs_init_meta_viewer_plugin() {
