f8af2c70366b495c9b84026e7c7d8a9d120c1bf4
Vladimir Sklyar <versus.post@gmail.com>
2021-05-29 18:58:09 +0300

    Shorten names of plugin's functions in PHP global namespace

--- meta-viewer.php
+++ meta-viewer.php
@@ -9,33 +9,33 @@ Global PHP prefix: vsm
 
 defined('ABSPATH') || exit;
 
-vsm_start_meta_viewer_plugin();
+vsm_start_plugin();
 
-function vsm_start_meta_viewer_plugin() {
+function vsm_start_plugin() {
 	if (! version_compare(PHP_VERSION, '7.3.0', '>=')) {
-		add_action('admin_notices', 'vsm_display_php_requirement_notice_for_meta_viewer_plugin');
+		add_action('admin_notices', 'vsm_display_php_requirement_notice');
 	}
 
 	global $wp_version;
 
 	if (! version_compare($wp_version, '5.6.4', '>=')) {
-		add_action('admin_notices', 'vsm_display_wp_core_requirement_notice_for_meta_viewer_plugin');
+		add_action('admin_notices', 'vsm_display_wp_core_requirement_notice');
 	}
 
-	add_action('admin_init', 'vsm_init_meta_viewer_plugin');
+	add_action('admin_init', 'vsm_init_plugin');
 }
 
-function vsm_display_php_requirement_notice_for_meta_viewer_plugin() {
+function vsm_display_php_requirement_notice() {
 	$message = 'The plugin "Meta viewer" doesn\'t support your PHP version and doesn\'t get initialized because of that.';
-	vsm_display_admin_notice_for_meta_viewer_plugin( $message );
+	vsm_display_admin_notice( $message );
 }
 
-function vsm_display_wp_core_requirement_notice_for_meta_viewer_plugin() {
+function vsm_display_wp_core_requirement_notice() {
 	$message = 'The plugin "Meta viewer" doesn\'t support your WordPress version and doesn\'t get initialized because of that.';
-	vsm_display_admin_notice_for_meta_viewer_plugin( $message );
+	vsm_display_admin_notice( $message );
 }
 
-function vsm_display_admin_notice_for_meta_viewer_plugin($message) {
+function vsm_display_admin_notice($message) {
 	if (! $message) {
 		return;
 	}
@@ -44,7 +44,7 @@ function vsm_display_admin_notice_for_meta_viewer_plugin($message) {
 	printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 }
 
-function vsm_init_meta_viewer_plugin() {
+function vsm_init_plugin() {
     require_once dirname(__FILE__) . '/inc/logic.php';
     require_once dirname(__FILE__) . '/init.php';
 }
