787c15f0b92ad6b8f264aa4259f17c5d4a26af52
Vladimir Sklyar <versus.post@gmail.com>
2021-05-30 11:18:36 +0300

    #6 Move css/js code to webpack...
    - Installed js-deps: babel, webpack, (s)css webpack loaders, node-sass
    - added package*.json files
    - js code has been extracted to a separate file
    - css code has been extracted to a separate file
    - css code has been transformed to SCSS syntax
    - css code is loaded in the js file (importing via webpack)

---
+++ assets/js/main.js
@@ -0,0 +1,119 @@
+import "../styles/main.scss";
+
+jQuery(document).ready(function($) {
+  var $metabox = $('.js-metaviewer-metabox');
+  var $header = $metabox.find('.js-metaviewer-metabox-header');
+  var $content = $metabox.find('.js-metaviewer-metabox-content');
+
+  $header.click(function () {
+    $metabox.toggleClass('js-metaviewer-metabox-closed');
+    handle_open_close_state();
+  });
+
+  var $data_table = $('.js-metaviewer-data');
+
+  var $pretty_links = $data_table.find('.js-pretty-code-button');
+  var $pretty_all_link = $data_table.find('.js-pretty-code-button-all');
+
+  var table = $data_table.stupidtable();
+
+  table.bind('aftertablesort', function (event, data) {
+    var arrow_class = 'vs-arrow';
+
+    var th = $(this).find("th");
+    th.find('.' + arrow_class).remove();
+
+    var dir = $.fn.stupidtable.dir;
+
+    var arrow_dir_class = data.direction === dir.ASC ? "vs-arrow_dir_up" : "vs-arrow_dir_down";
+    th.eq(data.column).append('<span class="' + arrow_class + ' ' + arrow_dir_class + '"></span>');
+  });
+
+  $pretty_links.click(function (e) {
+
+    var types = ['plain', 'pretty'];
+
+    var $target = $( e.target );
+    var prev_type = $target.attr('data-current-type');
+
+    var new_type = types.filter(function(item) {
+      return item !== prev_type;
+    })[0];
+
+    if (new_type === 'pretty') {
+      $target.addClass('vs-pretty-code-button_activated');
+    } else {
+      $target.removeClass('vs-pretty-code-button_activated');
+    }
+
+    $target.attr('data-current-type', new_type);
+
+    var $value_cell = $target.parent().siblings('.vs-table__column_content_value');
+    $value_cell
+      .find('[data-type="' + new_type + '"]')
+      .show()
+      .siblings()
+      .hide();
+  });
+
+  $pretty_all_link.click(function (e) {
+
+    var types = ['plain', 'pretty'];
+
+    var $target = $(e.target);
+    var previous_type = $target.attr('data-current-type');
+
+    var new_type = types.filter(function(item) {
+      return item !== previous_type;
+    })[0];
+
+    if (new_type === 'pretty') {
+      $target.addClass('vs-pretty-code-button_activated');
+    } else {
+      $target.removeClass('vs-pretty-code-button_activated');
+    }
+
+    $target.attr('data-current-type', new_type);
+
+    $pretty_links.attr('data-current-type', previous_type);
+    $pretty_links.trigger('click');
+  });
+
+  // Saving open/close metabox's state on entities' pages without Metabox API
+  var open_close_cookie_name = 'vs-metaviewer-metabox-closed-for-' + $metabox.attr('data-entity-type');
+  var open_close_cookie_values = ['opened', 'closed'];
+  var open_close_handler_enabled = false;
+  var open_close_handler_was_lauched = false;
+
+  if ($metabox.length) {
+    open_close_handler_enabled = true;
+    handle_open_close_state();
+  }
+
+  function handle_open_close_state() {
+
+    if (! open_close_handler_enabled) {
+      return;
+    }
+
+    var cookie_value = Cookies.get(open_close_cookie_name);
+
+    if (! jQuery.inArray(cookie_value, open_close_cookie_values)) {
+      cookie_value = 'opened';
+      Cookies.set(open_close_cookie_name, 'opened');
+    }
+
+    var current_state = $metabox.hasClass('js-metaviewer-metabox-closed') ? 'closed' : 'opened';
+
+    if (open_close_handler_was_lauched) {
+      Cookies.set(open_close_cookie_name, current_state);
+    } else {
+
+      open_close_handler_was_lauched = true;
+
+      if (current_state !== cookie_value && cookie_value === 'closed') {
+        $metabox.addClass('js-metaviewer-metabox-closed');
+      }
+    }
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// eslint-disable-next-line no-unused-vars
+module.exports = (env, argv) => {
+  const isModeProduction = env.mode === 'production';
+
+  const mode = isModeProduction ? 'production' : 'development';
+  console.log(`[META-VIEWER] Webpack mode = ${mode}`);
+
+  // Devtool
+  const sourceMaps = isModeProduction ? 'source-map' : 'inline-source-map';
+
+  // Plugins
+  const plugins = [];
+
+  plugins.push(new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+  }));
+
+  return {
+    mode,
+    entry: './assets/js/main.js',
+    output: {
+      filename: 'meta-viewer.build.js',
+      path: path.resolve(__dirname, 'assets/build'),
+    },
+    devtool: sourceMaps,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: '/node_modules/',
+          include: [
+            path.resolve(__dirname, 'assets/js'),
+          ],
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.(scss|css)$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    externals: {
+      jquery: 'jQuery',
+    },
+    plugins,
+  };
+};
