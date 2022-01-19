308c51c26a896a2ae9b794b2b2b61d209c212e98
Vladimir Sklyar <versus.post@gmail.com>
2022-01-19 19:05:13 +0300

    #9 Add basic tests for availability of the widget on admin edit pages of terms, users, comments

--- tests/acceptance/MiscCest.php
+++ tests/acceptance/MiscCest.php
@@ -23,4 +23,31 @@ class MiscCest
         $I->waitForElement('#vsm-post-meta', 1); // The root metabox
         $I->waitForElement('.vsm-refresh', 3); // "Refresh" button
     }
+
+    public function testTermMetabox(AcceptanceTester $I)
+    {
+        $I->loginAsAdmin();
+        $I->amOnPage('/wp-admin/term.php?taxonomy=category&tag_ID=1&post_type=post');
+
+        $I->waitForElement('#js-vsm-metabox', 1); // The root metabox
+        $I->waitForElement('.vsm-refresh', 3); // "Refresh" button
+    }
+
+    public function testUserMetabox(AcceptanceTester $I)
+    {
+        $I->loginAsAdmin();
+        $I->amOnPage('/wp-admin/profile.php');
+
+        $I->waitForElement('#js-vsm-metabox', 1); // The root metabox
+        $I->waitForElement('.vsm-refresh', 3); // "Refresh" button
+    }
+
+    public function testCommentMetabox(AcceptanceTester $I)
+    {
+        $I->loginAsAdmin();
+        $I->amOnPage('/wp-admin/comment.php?action=editcomment&c=2');
+
+        $I->waitForElement('#vsm-comment-meta', 1); // The root metabox
+        $I->waitForElement('.vsm-refresh', 3); // "Refresh" button
+    }
 }
