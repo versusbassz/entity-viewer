fa22d95d0f9e97601479ecc235d900eb5e9d4df4
Vladimir Sklyar <versus.post@gmail.com>
2022-01-20 20:19:13 +0300

    #8 #18 #17 Add details to phrases in the admin notices about incompatible PHP/WP versions + add related PHP-constants...
    - added PHP-constant ENTITY_VIEWER_MIN_PHP_VERSION
    - added PHP-constant ENTITY_VIEWER_MIN_WP_VERSION

--- entity-viewer.php
+++ entity-viewer.php
@@ -13,18 +13,20 @@ defined('ABSPATH') || exit;
 
 define('ENTITY_VIEWER_PLUGIN_VERSION',  '0.3.0-alpha');
 define('ENTITY_VIEWER_ENTRY_FILE_PATH', __FILE__);
+define('ENTITY_VIEWER_MIN_PHP_VERSION', '7.4.0');
+define('ENTITY_VIEWER_MIN_WP_VERSION', '5.7.0');
 
 vsm_start_plugin();
 
 function vsm_start_plugin() {
-    $supported_php_version = version_compare(PHP_VERSION, '7.4.0', '>=');
+    $supported_php_version = version_compare(PHP_VERSION, ENTITY_VIEWER_MIN_PHP_VERSION, '>=');
 
     if (! $supported_php_version) {
         add_action('admin_notices', 'vsm_display_php_requirement_notice');
     }
 
     global $wp_version;
-    $supported_wp_version = version_compare($wp_version, '5.7.0', '>=');
+    $supported_wp_version = version_compare($wp_version, ENTITY_VIEWER_MIN_WP_VERSION, '>=');
 
     if (! $supported_wp_version) {
         add_action('admin_notices', 'vsm_display_wp_core_requirement_notice');
@@ -39,11 +41,15 @@ function vsm_start_plugin() {
 
 function vsm_display_php_requirement_notice() {
     $message = esc_html__('The plugin "Entity viewer" doesn\'t support your PHP version and doesn\'t get initialized because of that.', 'entity-viewer');
+    $message .= ' ';
+    $message .= sprintf(esc_html__('The minimum required PHP version is: %s', 'entity-viewer'), ENTITY_VIEWER_MIN_PHP_VERSION);
     vsm_display_admin_notice($message);
 }
 
 function vsm_display_wp_core_requirement_notice() {
     $message = esc_html__('The plugin "Entity viewer" doesn\'t support your WordPress version and doesn\'t get initialized because of that.', 'entity-viewer');
+    $message .= ' ';
+    $message .= sprintf(esc_html__('The minimum required WordPress version is: %s', 'entity-viewer'), ENTITY_VIEWER_MIN_WP_VERSION);
     vsm_display_admin_notice($message);
 }
 
